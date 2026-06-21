35f6c02f00a17061fec21714a78215879a8292a7
Archiker-715 <serw144@gmail.com>
2026-06-21 11:38:24 +0300

    add cfg, logger

---
+++ cmd/sso/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/Archiker-715/grpc/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := initLogger(cfg.Env)
+
+	log.Info("starting app", slog.String("env", cfg.Env))
+}
+
+func initLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/Archiker-715/grpc
+
+go 1.25.0
+
+require (
+	github.com/Archiker-715/protos v0.0.0-20260620161202-4f1420aa6b2f // indirect
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/fatih/color v1.19.0 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.42.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
