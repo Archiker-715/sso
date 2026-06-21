e977138f390295c8d1bfe46f7baadd7529d4ac2a
Archiker-715 <serw144@gmail.com>
2026-06-21 19:06:03 +0300

    realize auth methods

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/Archiker-715/grpc/internal/app"
 	"github.com/Archiker-715/grpc/internal/config"
@@ -15,6 +17,8 @@ const (
 	envProd  = "prod"
 )
 
+// базу тоже нужно будет обернуть в run/stop
+
 func main() {
 	cfg := config.MustLoad()
 
@@ -24,7 +28,16 @@ func main() {
 
 	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
 
-	application.GRPCSrv.MustRun()
+	go application.GRPCSrv.MustRun()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	signal := <-stop
+	log.Info("stopping application", slog.String("signal", signal.String()))
+
+	application.GRPCSrv.Stop()
+	log.Info("application stopped")
 }
 
 func initLogger(env string) *slog.Logger {
